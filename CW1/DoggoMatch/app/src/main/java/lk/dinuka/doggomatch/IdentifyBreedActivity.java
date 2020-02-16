10a60a9040e5270e80ac052c1b85e084db6bc2f0
Dinuka <drpiyadigama@gmail.com>
2020-02-16 23:38:09 +0530

    Completed layout for Identify Breed game

---
+++ CW1/DoggoMatch/app/src/main/java/lk/dinuka/doggomatch/IdentifyBreedActivity.java
@@ -0,0 +1,14 @@
+package lk.dinuka.doggomatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdentifyBreedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify_breed);
+    }
+}
