1c7640a782f40da5bf2a1e90c6f613bf5fb81170
Dinuka <drpiyadigama@gmail.com>
2020-02-17 00:32:02 +0530

    Identify Dog Activity UI done

---
+++ CW1/DoggoMatch/app/src/main/java/lk/dinuka/doggomatch/SearchBreedActivity.java
@@ -0,0 +1,14 @@
+package lk.dinuka.doggomatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchBreedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_breed);
+    }
+}
