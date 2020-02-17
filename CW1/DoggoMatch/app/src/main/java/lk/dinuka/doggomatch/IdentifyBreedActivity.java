873cebfbdca3b13f6ddca8f754ac2aaecd104c35
Dinuka <drpiyadigama@gmail.com>
2020-02-17 18:39:34 +0530

    Worked on First game

--- CW1/DoggoMatch/app/src/main/java/lk/dinuka/doggomatch/IdentifyBreedActivity.java
+++ CW1/DoggoMatch/app/src/main/java/lk/dinuka/doggomatch/IdentifyBreedActivity.java
@@ -3,8 +3,15 @@ package lk.dinuka.doggomatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class IdentifyBreedActivity extends AppCompatActivity {
+public class IdentifyBreedActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    int imagesOldEnglishSheep[] = {R.drawable.n02105641_764};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,47 @@ public class IdentifyBreedActivity extends AppCompatActivity {
         catch (NullPointerException e){}
 
         setContentView(R.layout.activity_identify_breed);
+
+
+
+        // create the spinner
+        Spinner spinner = findViewById(R.id.breed_spinner);
+
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.breeds_array, android.R.layout.simple_spinner_item);
+
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);             // used to check whether the correct spinner item was identified. <Can remove later>
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void displayToast(String message) {          // used to check whether the spinner works as expected
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
